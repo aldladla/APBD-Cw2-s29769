6796a5df2f98bf49800eb3ee4a3a095c12497fc8
Aleksander Kostka <s29769@pjwstk.edu.pl>
2026-03-22 15:23:43 +0100

    Dodanie danych i metod w wypozyczenie

--- Projekt_obiektowy_w_C_s29769/Wypozyczenie.cs
+++ Projekt_obiektowy_w_C_s29769/Wypozyczenie.cs
@@ -2,5 +2,20 @@
 
 public class Wypozyczenie
 {
+    public Guid Id { get; private set; }
+    public Uzytkownik Wypozyczajacy { get; set; }
+    public Sprzet WypozyczanySprzet { get; set; }
+    public DateTime DataWypozyczenia { get; set; }
+    public DateTime PlanowanaDataZwrotu { get; set; } 
+    public DateTime? FaktycznaDataZwrotu { get; set; } 
+    public decimal KaraZaOpoznienie { get; set; } 
     
+    public Wypozyczenie(Uzytkownik wypozyczajacy, Sprzet sprzet, int liczbaDni)
+    {
+        this.Id = Guid.NewGuid();
+        this.Wypozyczajacy = wypozyczajacy;
+        this.WypozyczanySprzet = sprzet;
+        this.DataWypozyczenia = DateTime.Now;
+        this.PlanowanaDataZwrotu = DateTime.Now.AddDays(liczbaDni);
+    }
 }
