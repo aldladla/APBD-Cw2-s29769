ca973fa51de635a198cc31a6c5b0d1abd9ce11c7
Aleksander Kostka <s29769@pjwstk.edu.pl>
2026-03-23 16:46:06 +0100

    dodanie reszty metod w wypozyczenieserwis

--- Projekt_obiektowy_w_C_s29769/WypozyczenieSerwis.cs
+++ Projekt_obiektowy_w_C_s29769/WypozyczenieSerwis.cs
@@ -18,53 +18,152 @@ public class WypozyczalniaSerwis
 
     public void WypozyczSprzet(Guid uzytkownikId, Guid sprzetId, int liczbaDni)
     {
-        Uzytkownik uzytkownik = null;
+        Uzytkownik znalezionyUzytkownik = null;
         foreach (var u in _uzytkownicy)
         {
             if (u.Id == uzytkownikId)
             {
-                uzytkownik = u;
-                break;
+                znalezionyUzytkownik = u;
             }
         }
 
-        Sprzet sprzet = null;
+        Sprzet znalezionySprzet = null;
         foreach (var s in _sprzet)
         {
             if (s.Id == sprzetId)
             {
-                sprzet = s;
-                break;
+                znalezionySprzet = s;
             }
         }
 
-        if (uzytkownik == null || sprzet == null)
+        if (znalezionyUzytkownik == null || znalezionySprzet == null)
         {
-            throw new ArgumentException("Nie znaleziono użytkownika lub sprzetu w systemie");
+            throw new Exception("Nie ma takiego uzytkownika lub sprzetu.");
         }
 
-        if (sprzet.Status != StatusDostepnosci.Dostepne)
+        if (znalezionySprzet.Status != StatusDostepnosci.Dostepne)
         {
-            throw new InvalidOperationException("Sprzet nie jest obecnie dostepny do wypozyczenia");
+            throw new Exception("Ten sprzet jest aktualnie zajety.");
         }
 
-        int liczbaAktywnychWypozyczen = 0;
+        int liczbaAktywnych = 0;
         foreach (var w in _wypozyczenia)
         {
-            if (w.Wypozyczajacy.Id == uzytkownik.Id && w.FaktycznaDataZwrotu == null)
+            if (w.Wypozyczajacy.Id == uzytkownikId)
             {
-                liczbaAktywnychWypozyczen++;
+                if (w.FaktycznaDataZwrotu == null)
+                {
+                    liczbaAktywnych++;
+                }
             }
         }
 
-        if (liczbaAktywnychWypozyczen >= uzytkownik.LimitWypozyczen)
+        if (liczbaAktywnych >= znalezionyUzytkownik.LimitWypozyczen)
         {
-            throw new InvalidOperationException($"Użytkownik osiagnal swoj limit ({uzytkownik.LimitWypozyczen}) aktywnych wypozyczen");
+            throw new Exception("Ten uzytkownik ma juz za duzo wypozyczonego sprzetu.");
         }
 
-        var noweWypozyczenie = new Wypozyczenie(uzytkownik, sprzet, liczbaDni);
+        Wypozyczenie noweWypozyczenie = new Wypozyczenie(znalezionyUzytkownik, znalezionySprzet, liczbaDni);
         _wypozyczenia.Add(noweWypozyczenie);
         
-        sprzet.Status = StatusDostepnosci.Wypozyczony;
+        znalezionySprzet.Status = StatusDostepnosci.Wypozyczony;
+    }
+
+    public void ZwrocSprzet(Guid sprzetId)
+    {
+        Wypozyczenie doZwrotu = null;
+        foreach (var w in _wypozyczenia)
+        {
+            if (w.WypozyczanySprzet.Id == sprzetId)
+            {
+                if (w.FaktycznaDataZwrotu == null)
+                {
+                    doZwrotu = w;
+                }
+            }
+        }
+
+        if (doZwrotu != null)
+        {
+            doZwrotu.FaktycznaDataZwrotu = DateTime.Now;
+            doZwrotu.WypozyczanySprzet.Status = StatusDostepnosci.Dostepne;
+
+            if (doZwrotu.FaktycznaDataZwrotu > doZwrotu.PlanowanaDataZwrotu)
+            {
+                TimeSpan roznica = (DateTime)doZwrotu.FaktycznaDataZwrotu - doZwrotu.PlanowanaDataZwrotu;
+                int dniSpoznienia = roznica.Days;
+                
+                if (dniSpoznienia > 0)
+                {
+                    doZwrotu.KaraZaOpoznienie = dniSpoznienia * 10; 
+                }
+            }
+        }
+    }
+
+    public void OznaczUszkodzony(Guid sprzetId)
+    {
+        foreach (var s in _sprzet)
+        {
+            if (s.Id == sprzetId)
+            {
+                s.Status = StatusDostepnosci.Niedostepne;
+            }
+        }
+    }
+
+    public List<Sprzet> PobierzCalySprzet()
+    {
+        return _sprzet;
+    }
+
+    public List<Uzytkownik> PobierzWszystkichUzytkownikow()
+    {
+        return _uzytkownicy;
+    }
+    
+    public List<Sprzet> PobierzDostepnySprzet()
+    {
+        List<Sprzet> dostepne = new List<Sprzet>();
+        foreach (var s in _sprzet)
+        {
+            if (s.Status == StatusDostepnosci.Dostepne)
+            {
+                dostepne.Add(s);
+            }
+        }
+        return dostepne;
+    }
+        
+    public List<Wypozyczenie> PobierzAktywneWypozyczenia(Guid uzytkownikId)
+    {
+        List<Wypozyczenie> aktywne = new List<Wypozyczenie>();
+        foreach (var w in _wypozyczenia)
+        {
+            if (w.Wypozyczajacy.Id == uzytkownikId)
+            {
+                if (w.FaktycznaDataZwrotu == null)
+                {
+                    aktywne.Add(w);
+                }
+            }
+        }
+        return aktywne;
+    }
+        
+    public List<Wypozyczenie> PobierzPrzeterminowane()
+    {
+        List<Wypozyczenie> przeterminowane = new List<Wypozyczenie>();
+        foreach (var w in _wypozyczenia)
+        {
+            if (w.FaktycznaDataZwrotu == null)
+            {
+                if (DateTime.Now > w.PlanowanaDataZwrotu)
+                {
+                    przeterminowane.Add(w);
+                }
+            }
+        }
+        return przeterminowane;
     }
 }
